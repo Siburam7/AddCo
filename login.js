9dfe42e727c668958b48d1fbe88029f5abe67d47
gaming-cbu <siburamnayak18@.com>
2026-08-22 23:16:41 +0530

    Add SVG

--- login.js
+++ login.js
@@ -5,3 +5,20 @@ let loginBtn = document.querySelector(".btn-login");
 loginBtn.addEventListener("click", () => {
   window.location.href = "main.html";
 });
+
+// Eye In Password
+
+let password = document.querySelector(".password-box input");
+let eye = document.querySelector(".password-box i");
+
+eye.addEventListener("click", () => {
+  if (password.type === "password") {
+    password.type = "text";
+    eye.classList.remove("fa-eye");
+    eye.classList.add("fa-eye-slash");
+  } else {
+    password.type = "password";
+    eye.classList.remove("fa-eye-slash");
+    eye.classList.add("fa-eye");
+  }
+});
