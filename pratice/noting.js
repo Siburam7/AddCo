e1ae71c7c25d3d42f506dab624cf7afdd1664622
gaming-cbu <siburamnayak18@.com>
2026-08-25 00:19:40 +0530

    All Page Acc

--- pratice/noting.js
+++ pratice/noting.js
@@ -43,43 +43,41 @@ addProductToggle();
 
 // nav-menu
 
-let navMenuToggle = function () {
-  let buttomNav = document.querySelector(".nav-button");
-  let manuBtn = document.querySelector(".fa-bars");
-  let manuBtnX = document.querySelector(".fa-x");
-  let navBtnText = document.querySelector(".text-top-nav-buttom");
+let buttomNav = document.querySelector(".nav-button");
+let manuBtn = document.querySelector(".fa-bars");
+let manuBtnX = document.querySelector(".fa-x");
+let navBtnText = document.querySelector(".text-top-nav-buttom");
 
-  let OrgTetxAll = document.querySelector(".manu-allText");
-  let toggleTextAll = document.querySelector(".toggle-manu-allText");
+let OrgTetxAll = document.querySelector(".manu-allText");
+let toggleTextAll = document.querySelector(".toggle-manu-allText");
 
+manuBtnX.style.display = "none";
+
+manuBtn.addEventListener("click", () => {
+  manuBtnX.style.display = "flex";
+  buttomNav.style.height = "calc(100vh - 45px)";
+  buttomNav.style.backgroundColor = "white";
+  buttomNav.style.width = "75%";
+  navBtnText.style.display = "none";
+
+  OrgTetxAll.classList.toggle("toggle-manu-allText");
+  manuBtn.style.display = "none";
+  document.body.style.overflow = "hidden";
+});
+
+manuBtnX.addEventListener("click", () => {
+  manuBtn.style.display = "flex";
+  buttomNav.style.height = "28px";
+  buttomNav.style.backgroundColor = "rgba(25, 7, 162, 0.304)";
+  buttomNav.style.width = "100%";
+  navBtnText.style.display = "inline";
+
+  OrgTetxAll.classList.toggle("toggle-manu-allText");
   manuBtnX.style.display = "none";
+  document.body.style.overflow = "auto";
+});
 
-  manuBtn.addEventListener("click", () => {
-    manuBtnX.style.display = "flex";
-    buttomNav.style.height = "calc(100vh - 45px)";
-    buttomNav.style.backgroundColor = "white";
-    buttomNav.style.width = "75%";
-    navBtnText.style.display = "none";
-
-    OrgTetxAll.classList.toggle("toggle-manu-allText");
-    manuBtn.style.display = "none";
-    document.body.style.overflow = "hidden";
-  });
-
-  manuBtnX.addEventListener("click", () => {
-    manuBtn.style.display = "flex";
-    buttomNav.style.height = "28px";
-    buttomNav.style.backgroundColor = "rgba(25, 7, 162, 0.304)";
-    buttomNav.style.width = "100%";
-    navBtnText.style.display = "inline";
-
-    OrgTetxAll.classList.toggle("toggle-manu-allText");
-    manuBtnX.style.display = "none";
-    document.body.style.overflow = "auto";
-  });
-};
-
-navMenuToggle();
+// navMenuToggle();
 
 // Footer all Option
 
@@ -88,16 +86,72 @@ let footerMinGroup = document.querySelectorAll(".footerMinGroup");
 
 footerMin.forEach((fMin, idx) => {
   fMin.addEventListener("click", () => {
-    footerMin[0].classList.remove("active-footer");
-    footerMin[1].classList.remove("active-footer");
-    footerMin[2].classList.remove("active-footer");
-    footerMin[3].classList.remove("active-footer");
+    footerMin.forEach((fMinX) => {
+      fMinX.classList.remove("active-footer");
+    });
+
     fMin.classList.add("active-footer");
 
-    footerMinGroup[0].style.display = "none";
-    footerMinGroup[1].style.display = "none";
-    footerMinGroup[2].style.display = "none";
-    footerMinGroup[3].style.display = "none";
+    footerMinGroup.forEach((fMinGroup) => {
+      fMinGroup.style.display = "none";
+    });
     footerMinGroup[idx].style.display = "flex";
+
+    //
+
+    for (let i = 0; i < navContainer.length; i++) {
+      navContainer[i].classList.remove("toogleNavBottom");
+    }
+
+    navContainer[idx].classList.add("toogleNavBottom");
+  });
+});
+
+// Nav bottom Selector
+
+let navContainer = document.querySelectorAll(".manu-allText h1");
+let navContainerToggle = document.querySelector(".toogleNavBottom");
+
+navContainer.forEach((navSingle, idx) => {
+  navSingle.addEventListener("click", () => {
+    if (idx === navContainer.length - 1) {
+      return;
+    }
+
+    for (let i = 0; i < navContainer.length; i++) {
+      navContainer[i].classList.remove("toogleNavBottom");
+    }
+    navSingle.classList.add("toogleNavBottom");
+
+    //
+
+    footerMinGroup.forEach((fMinGroup) => {
+      fMinGroup.style.display = "none";
+    });
+    footerMinGroup[idx].style.display = "flex";
+
+    //
+
+    if (idx < footerMin.length) {
+      footerMin.forEach((fMinX) => {
+        fMinX.classList.remove("active-footer");
+      });
+
+      footerMin[idx].classList.add("active-footer");
+    }
+
+    //
+
+    setTimeout(() => {
+      manuBtn.style.display = "flex";
+      buttomNav.style.height = "28px";
+      buttomNav.style.backgroundColor = "rgba(25, 7, 162, 0.304)";
+      buttomNav.style.width = "100%";
+      navBtnText.style.display = "inline";
+
+      OrgTetxAll.classList.toggle("toggle-manu-allText");
+      manuBtnX.style.display = "none";
+      document.body.style.overflow = "auto";
+    }, 150);
   });
 });
