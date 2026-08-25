328b493a521846dea7072bf8fb05525796e6dcca
gaming-cbu <siburamnayak18@.com>
2026-08-25 23:50:57 +0530

    Add Local Store

--- pratice/noting.js
+++ pratice/noting.js
@@ -1,46 +1,3 @@
-let allData = function () {
-  let allDetails = {};
-
-  let form = document.querySelector("form");
-
-  form.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    let firstName = document.querySelector("#username").value;
-    let productName = document.querySelector("#productName").value;
-    let productPrice = document.querySelector("#productPrice").value;
-
-    if ([firstName] in allDetails) {
-      allDetails[firstName].push([productName, productPrice]);
-    } else {
-      allDetails[firstName] = [[productName, productPrice]];
-    }
-
-    console.log(allDetails);
-  });
-};
-
-allData();
-
-// add product toggle
-
-let addProductToggle = function () {
-  let addBtn = document.querySelector("#add-product-text");
-  let addForm = document.querySelector(".add-product");
-
-  addBtn.addEventListener("click", () => {
-    if (addForm.style.display === "none") {
-      addForm.style.display = "flex";
-      addBtn.style.color = "rgba(25, 7, 162, 0.634)";
-    } else {
-      addForm.style.display = "none";
-      addBtn.style.color = "black";
-    }
-  });
-};
-
-addProductToggle();
-
 // nav-menu
 
 let buttomNav = document.querySelector(".nav-button");
@@ -84,6 +41,39 @@ manuBtnX.addEventListener("click", () => {
 let footerMin = document.querySelectorAll(".footer-min");
 let footerMinGroup = document.querySelectorAll(".footerMinGroup");
 
+let navContainer = document.querySelectorAll(".manu-allText h1");
+let navContainerToggle = document.querySelector(".toogleNavBottom");
+
+// restore last page
+
+let lastPage = localStorage.getItem("lastPage");
+console.log(lastPage);
+
+footerMinGroup.forEach((fMinX) => {
+  fMinX.style.display = "none";
+});
+
+if (lastPage !== null) {
+  lastPage = Number(lastPage);
+
+  footerMinGroup[lastPage].style.display = "flex";
+  for (let i = 0; i < navContainer.length; i++) {
+    navContainer[i].classList.remove("toogleNavBottom");
+  }
+
+  navContainer[lastPage].classList.add("toogleNavBottom");
+
+  for (let i = 0; i < footerMin.length; i++) {
+    footerMin[i].classList.remove("active-footer");
+  }
+
+  if (lastPage < 4) {
+    footerMin[lastPage].classList.add("active-footer");
+  }
+}
+
+// restore last page end here
+
 footerMin.forEach((fMin, idx) => {
   fMin.addEventListener("click", () => {
     footerMin.forEach((fMinX) => {
@@ -96,6 +86,7 @@ footerMin.forEach((fMin, idx) => {
       fMinGroup.style.display = "none";
     });
     footerMinGroup[idx].style.display = "flex";
+    localStorage.setItem("lastPage", idx);
 
     //
 
@@ -109,9 +100,6 @@ footerMin.forEach((fMin, idx) => {
 
 // Nav bottom Selector
 
-let navContainer = document.querySelectorAll(".manu-allText h1");
-let navContainerToggle = document.querySelector(".toogleNavBottom");
-
 navContainer.forEach((navSingle, idx) => {
   navSingle.addEventListener("click", () => {
     if (idx === navContainer.length - 1) {
@@ -122,6 +110,7 @@ navContainer.forEach((navSingle, idx) => {
       navContainer[i].classList.remove("toogleNavBottom");
     }
     navSingle.classList.add("toogleNavBottom");
+    localStorage.setItem("lastPage", idx);
 
     //
 
@@ -132,11 +121,11 @@ navContainer.forEach((navSingle, idx) => {
 
     //
 
-    if (idx < footerMin.length) {
-      footerMin.forEach((fMinX) => {
-        fMinX.classList.remove("active-footer");
-      });
+    footerMin.forEach((fMinX) => {
+      fMinX.classList.remove("active-footer");
+    });
 
+    if (idx < 4) {
       footerMin[idx].classList.add("active-footer");
     }
 
